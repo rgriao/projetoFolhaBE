25b89d28d4c8e1ee7d4a67909f5fb425c9d28f18
rgriao <92741051+rgriao@users.noreply.github.com>
2022-03-19 19:59:17 -0300

    Sequelize

--- app/models/index.js
+++ app/models/index.js
@@ -1,39 +1,19 @@
-const dbConfig = require("../config/db.config.js");
-const Sequelize = require("sequelize");
-//conecta ao banco de dados
-
-const sequelize = new Sequelize(`${process.env.DATABASE}`, `${process.env.DB_USER}`, `${process.env.PASSWORD}`, 
-{
-  host: process.env.HOST,
-  dialect: 'postgres'
-});
-/*const sequelize = new Sequelize("DATABASE", "DB_USER", "PASSWORD",
-
-const sequelize = new Sequelize("postgres://postgres:postgres@localhost/desi6576mj2kdf", {
-//    dialect: 'postgres'
-//});  
-  {    
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-  define: {
-    timestamps: false
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-});
+);
 
-sequelize.beforeConnect(async (currConfig) => {
-  try {
-      await sequelize.authenticate();
-      console.log('A conexão foi estabelecida com sucesso!');
-  } catch (error) {
-      console.error('Não foi possível se conectar à base de dados!', error);
-  }
-});*/
-
-//cria a tabela real conforme modelo
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-db.dadosfolhas = require("./CamposFolha.js")(sequelize, Sequelize);
-
-module.exports = db;
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
