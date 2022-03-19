fb49cec0b0874454bbf5e70072e9ee850c839002
rgriao <92741051+rgriao@users.noreply.github.com>
2022-03-19 20:14:36 -0300

    PG_USER

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,28 +1,31 @@
-/*Antes de começar a configurar o Sequelize no projeto devemos iniciá-lo
-,ou seja, criar os arquivos iniciais de configuração, e para isso 
-executamos o comando:
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+//PG_HOST = 127.0.0.1
 
-./node_modules/.bin/sequelize init
-ou
-npx sequelize init
-
-Esse processo deve criar alguns arquivos no projeto, como a pasta config, 
-migrations, models e seeders.
-Renomeamos o arquivo config/config.json para 
-configurações/db.config.js e colocamos o código abaixo:
-https://blog.rocketseat.com.br/nodejs-express-sequelize/*/
-
+console.log(process.env.DATABASE_URL);
 module.exports = {
-    HOST: process.env.DATABASE_URL,
-    //USER: process.env.DB_USER,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_DATABASE: process.env.DB_DATABASE,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  development: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    dialect: 'postgres',
+    port: process.env.PG_PORT,
+    operatorsAliases: 0,
+  },
+  test: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    dialect: 'postgres',
+    port: process.env.PG_PORT,
+    operatorsAliases: 0,
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
+  },
+};
