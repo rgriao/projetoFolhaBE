64fbeefe20a0d0e97cd468db3b97d1f2f4aa908d
rgriao <92741051+rgriao@users.noreply.github.com>
2022-04-09 19:03:42 -0300

    terc terntativa .env 09/04/2022

--- app/config/configDB.js
+++ app/config/configDB.js
@@ -24,7 +24,7 @@ module.exports = {
     dialect: 'postgres',   
   },
   production: {
-    database: process.env.DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: true,
