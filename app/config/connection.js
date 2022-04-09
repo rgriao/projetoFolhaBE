2e7df8c982f2c94c67207cb94d196dcba33cdd05
rgriao <92741051+rgriao@users.noreply.github.com>
2022-04-09 19:45:46 -0300

    Alteração .env 09/04/2022

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,17 +1,26 @@
 const Sequelize = require("sequelize");
 const dbConfig = require('./configDB');
-      
+var conn = "";
 exports.conexao = () => {
-
  if (process.env.NODE_ENV.trim() === 'development' || process.env.NODE_ENV.trim() === 'test') {    
-  const conn = new Sequelize(dbConfig.development.database, dbConfig.development.user, 
+  conn = new Sequelize(dbConfig.development.database, dbConfig.development.user, 
    dbConfig.development.password, {         
    host: dbConfig.development.host,
    dialect: 'postgres',  
    define: {
      timestamps: false
      }  
-   })    
-   return (conn);
- }; 
+   })  
+ }else{
+  conn = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+   }
+  );
+ }
+ return (conn);
 }
